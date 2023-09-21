198eb01374469d5af0f0d9f22e5902ec4dc1810f
sabela <sabela@a21eql00.sanclemente.local>
2023-09-21 14:09:35 +0200

    first commit

---
+++ UD01_1Primer_proyecto/app/src/main/java/com/example/ud01_1primer_proyecto/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.ud01_1primer_proyecto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
