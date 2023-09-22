dfaf1f6c0e2fe1e8ac5635666302470894122948
sabela <sabela@a21eql00.sanclemente.local>
2023-09-22 14:04:05 +0200

    proyecto 2

--- UD01_2One_More_Wine/app/src/main/java/com/example/ud01_2one_more_wine/MainActivity.kt
+++ UD01_2One_More_Wine/app/src/main/java/com/example/ud01_2one_more_wine/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.ud01_2one_more_wine
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Spinner
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val buttonSearch = findViewById<Button>(R.id.button)
+        buttonSearch.setOnClickListener {
+            val spinnerWineTypes = findViewById<Spinner>(R.id.wineType)
+            val textTypesWines = findViewById<TextView>(R.id.textTypesWines)
+            textTypesWines.text = getWines(spinnerWineTypes.selectedItemId).joinToString( "/n")
+        }
+
+    }
+
+    fun getWines(id: Long) : List<String> = when(id){
+        0L -> listOf("Albariño", "Moscato", "Verdejo")
+        1L -> listOf("Rioja", "Ribera del duero", "Toro")
+        2L -> listOf("Cabernet", "Franc", "Merlot")
+        else -> listOf()
     }
 }
