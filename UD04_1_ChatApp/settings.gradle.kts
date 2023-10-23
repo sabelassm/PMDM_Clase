1524a1a7f4d15b83699a8eb4e6cad48dc941965e
sabela <sabela@a21eql00.sanclemente.local>
2023-10-23 14:28:15 +0200

    chatapp

---
+++ UD04_1_ChatApp/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "UD04_1_ChatApp"
+include(":app")
+ 
