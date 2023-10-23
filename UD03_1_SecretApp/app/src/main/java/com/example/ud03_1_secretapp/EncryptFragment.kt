a0e508b8a82e6faf5cb2b83b3ff2f2c99ea2afe6
sabela <sabela@a21eql00.sanclemente.local>
2023-10-23 13:40:42 +0200

    Secret App

--- UD03_1_SecretApp/app/src/main/java/com/example/ud03_1_secretapp/EncryptFragment.kt
+++ UD03_1_SecretApp/app/src/main/java/com/example/ud03_1_secretapp/EncryptFragment.kt
@@ -16,12 +16,15 @@ class EncryptFragment : Fragment() {
         // Inflate the layout for this fragment
         val message = EncryptFragmentArgs.fromBundle(requireArguments()).message
         val view = inflater.inflate(R.layout.fragment_encrypt, container, false)
-        view.findViewById<TextView>(R.id.encrypt_textValue).text = message
+        view.findViewById<TextView>(R.id.encrypt_textValue).text = cifrado(message)
         return view
     }
 
     fun cifrado(message: String) = message.map {
-
-    }
+        if(it.isLetter())
+            it.uppercaseChar().code.minus('A'.code).plus(3).mod(26).plus('A'.code).toChar()
+        else
+            it
+    }.joinToString("")
 
 }
