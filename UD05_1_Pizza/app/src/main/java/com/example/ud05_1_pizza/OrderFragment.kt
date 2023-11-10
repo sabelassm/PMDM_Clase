dc1f5b5f1ce148df2dd909ef33be95de9ac54d35
sabela <sabela@a21eql00.sanclemente.local>
2023-11-10 12:32:39 +0100

    pizza

--- UD05_1_Pizza/app/src/main/java/com/example/ud05_1_pizza/OrderFragment.kt
+++ UD05_1_Pizza/app/src/main/java/com/example/ud05_1_pizza/OrderFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.appbar.MaterialToolbar
 
@@ -25,6 +27,17 @@ class OrderFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_order, container, false)
         val toolbar = view.findViewById<MaterialToolbar>(R.id.toolbar)
         (activity as AppCompatActivity).setSupportActionBar(toolbar)
+
+        val pizzaGroup = view.findViewById<RadioGroup>(R.id.group_pizza_type)
+        val pizzaType = pizzaGroup.checkedRadioButtonId
+        var msg =""
+        if(pizzaType==-1){
+            msg = "Debes seleccionar un tipo de pizza"
+            Toast.makeText(activity, msg, Toast.LENGTH_LONG).show()
+        }else{
+
+        }
+
         return view
     }
 
