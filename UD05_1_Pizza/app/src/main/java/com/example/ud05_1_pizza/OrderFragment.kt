585afa02a7f9063980ae09c75898b837b877e896
sabela <sabela@a21eql00.sanclemente.local>
2023-11-10 14:27:49 +0100

    1_Pizza terminado

--- UD05_1_Pizza/app/src/main/java/com/example/ud05_1_pizza/OrderFragment.kt
+++ UD05_1_Pizza/app/src/main/java/com/example/ud05_1_pizza/OrderFragment.kt
@@ -9,6 +9,9 @@ import android.widget.RadioGroup
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.chip.Chip
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 
 /**
  * A simple [Fragment] subclass.
@@ -28,16 +31,38 @@ class OrderFragment : Fragment() {
         val toolbar = view.findViewById<MaterialToolbar>(R.id.toolbar)
         (activity as AppCompatActivity).setSupportActionBar(toolbar)
 
-        val pizzaGroup = view.findViewById<RadioGroup>(R.id.group_pizza_type)
-        val pizzaType = pizzaGroup.checkedRadioButtonId
-        var msg =""
-        if(pizzaType==-1){
-            msg = "Debes seleccionar un tipo de pizza"
-            Toast.makeText(activity, msg, Toast.LENGTH_LONG).show()
-        }else{
+
+        val fabNext = view.findViewById<FloatingActionButton>(R.id.fab)
+        fabNext.setOnClickListener {
+            val pizzaGroup = view.findViewById<RadioGroup>(R.id.group_pizza_type)
+            val pizzaType = pizzaGroup.checkedRadioButtonId
+            var msg =""
+            if(pizzaType==-1){
+                msg = "Debes seleccionar un tipo de pizza"
+            }else{
+                msg ="Has seleccionado una pizza de tipo "
+                //Tipo de pizza
+                msg+= when(pizzaType){
+                    R.id.radio_margarita -> "Margarita"
+                    R.id.radio_calzone -> "Calzone"
+                    else -> "No existe"
+                }
+                //Extras
+                var parmesano = view.findViewById<Chip>(R.id.chip_parmesano)
+                if(parmesano.isChecked) msg+= " con pamersano"
+                var tomate = view.findViewById<Chip>(R.id.chip_tomate_cherry)
+                if(tomate.isChecked) msg+= " con tomate cherry"
+
+            }
+            //Toast.makeText(activity, msg, Toast.LENGTH_LONG).show()
+            val snackbar = Snackbar.make(fabNext, msg, Snackbar.LENGTH_SHORT)
+            snackbar.setAction("Undo"){
+                pizzaGroup.clearCheck()
+            }
+            snackbar.show()
+
 
         }
-
         return view
     }
 
