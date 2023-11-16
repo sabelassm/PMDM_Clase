3d8ad52ae534a66dee88fcc3d124d119f6a6c5be
sabela <sabela@a21eql00.sanclemente.local>
2023-11-16 13:13:45 +0100

    binding

--- UD06_2_SecretApp/app/src/main/java/com/example/ud03_1_secretapp/EncryptFragment.kt
+++ UD06_2_SecretApp/app/src/main/java/com/example/ud03_1_secretapp/EncryptFragment.kt
@@ -15,7 +15,7 @@ class EncryptFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         val message = EncryptFragmentArgs.fromBundle(requireArguments()).message
         _binding = FragmentEncryptBinding.inflate(inflater, container, false)
